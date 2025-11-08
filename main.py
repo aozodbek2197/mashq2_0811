806a604a4e0a892fcb361b320a71a716e7103ab8
aozodbek2197 <aozodbek2197@gmail.com>
2025-11-08 14:25:32 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# # 1-misol
+# satrlar = ["salom", "kitob", "dasturlash"]
+# uzunliklar = list(map(lambda s: len(s), satrlar))
+# print("6. Satr uzunliklari:", uzunliklar)
+
+# # 2-misol
+# sonlar = [1, 2, 3, 4, 5, 6, 7]
+# toq_sonlar = list(filter(lambda x: x % 2 != 0, sonlar))
+# toq_kvadratlar = list(map(lambda x: x ** 2, toq_sonlar))
+# print("7. Toq sonlar kvadrati:", toq_kvadratlar)
+
+# # 3-misol
+# royxat = [2, 4, 6, 8]
+# natija = list(map(lambda x: x[0] * x[1], enumerate(royxat)))
+# print("8. Indeksga ko‘paytirilgan:", natija)
+
+# # 4-misol
+# satrlar = ["level", "python", "madam", "salom"]
+# palindrom = list(filter(lambda s: s == s[::-1], satrlar))
+# print("9. Palindrom satrlar:", palindrom)
+
+# # 5-misol
+# sonlar = [1, 4, 5, 6, 8, 10, 12]
+# tanlanganlar = list(filter(lambda x: 5 < x < 10, sonlar))
+# natija = list(map(lambda x: x * 3, tanlanganlar))
+# print("10. 5–10 oralig‘i, 3 ga ko‘paytirilgan:", natija)
